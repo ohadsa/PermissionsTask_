12fb19a887130e0234cc8b0670b9163eec75f9a5
tedooo <ohad@tedooo.com>
2022-12-09 21:56:18 +0200

    aa

--- app/src/main/java/com/example/aouthentication/ui/components/utils/Extensions.kt
+++ app/src/main/java/com/example/aouthentication/ui/components/utils/Extensions.kt
@@ -1,7 +1,6 @@
 package com.example.aouthentication.ui.components.utils
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
